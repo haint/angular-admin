14845f2c2318fb826e474cad95662844acc23c1e
haint <haithanh0809@gmail.com>
2014-12-10 10:53:46 +0700

    layout module

---
+++ public/app/layout/module.js
@@ -0,0 +1,29 @@
+define([
+  'angular',
+  'angular-couch-potato',
+  'angular-ui-router'
+], function(ng, couchPotato) {
+  "use strict";
+
+  var module = ng.module('app.layout', ['ui.router']);
+
+  couchPotato.configureApp(module);
+
+  module.config(function ($stateProvider, $couchPotatoProvider, $urlRouterProvider) {
+    $stateProvider
+      .state('app', {
+        abstract: true,
+        views: {
+          root: {
+            templateUrl: 'app/layout/layout.tpl.html'
+          }
+        }
+      });
+  });
+
+  module.run(function($couchPotato) {
+    module.lazy = $couchPotato;
+  });
+
+  return module;
+})
