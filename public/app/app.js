11885ca74d440280acc13cb072854d48f2ed8b49
haint <haithanh0809@gmail.com>
2014-12-10 10:45:25 +0700

    add error interceptor for http request and response

--- public/app/app.js
+++ public/app/app.js
@@ -3,7 +3,9 @@ define([
   'angular-couch-potato',
   'angular-ui-router',
   'angular-animate',
-  'angular-bootstrap'
+  'angular-bootstrap',
+  'smartwidgets',
+  'notification'
 ], function(ng, couchPotato) {
 
   var app = ng.module('app', [
@@ -14,6 +16,54 @@ define([
   ]);
 
   couchPotato.configureApp(app);
+
+  app.config(function($provide, $httpProvider) {
+
+    //Intercept http calls.
+    $provide.factory('ErrorHttpInterceptor', function($q) {
+      var errorCounter = 0;
+      function notifyError(rejection) {
+        console.log(rejection);
+        $.bigBox({
+          title: rejection.status + ' ' + rejection.statusText,
+          content: rejection.data,
+          color: "#C46A69",
+          icon: "fa fa-warning shake animated",
+          number: ++errorCounter,
+          timeout: 6000
+        });
+      }
+
+      return {
+        //On request failure
+        requestError: function(rejection) {
+          //show notification
+          notifyError(rejection);
+
+          //return the promise rejection.
+          return $q.reject(rejection);
+        },
+
+        //On response failure
+        responseError: function(rejection) {
+          //show notification
+          notifyError(rejection);
+          //return the promise rejection.
+          return $q.reject(rejection);
+        }
+      };
+    });
+
+    //Add the interceptor to $httpProvider.
+    $httpProvider.interceptors.push('ErrorHttpInterceptor');
+
+  });
+
+  app.run(function($couchPotato, $rootScope, $state, $stateParams) {
+    app.lazy = $couchPotato;
+    $rootScope.$state = $state;
+    $rootScope.$stateParams = $stateParams;
+  });
 
   return app;
 });
