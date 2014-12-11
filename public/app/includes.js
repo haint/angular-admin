4e635bb04d073af080cc5ad1b67db6a48611806f
haint <haithanh0809@gmail.com>
2014-12-11 14:55:54 +0700

    Recent project in header

--- public/app/includes.js
+++ public/app/includes.js
@@ -7,9 +7,16 @@ define([
   'dashboard/module',
 
   //components
+
+  //activities
   'components/activities/activities-controller',
   'components/activities/activities-service',
   'components/activities/activities-dropdown-toggle-directive',
+
+  //projects
+  'components/projects/Projects',
+  'components/projects/recent-projects'
+
 ], function() {
   'use strict';
 });
